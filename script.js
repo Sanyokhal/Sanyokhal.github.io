e727f2c67e8016a87b665f093cab691fb1661cdf
Sanyokhal <62928715+Sanyokhal@users.noreply.github.com>
2020-11-13 17:26:12 +0200

    New Site

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-window.onscroll = function showHeader() {
-    var header = document.querySelector('.first-part__menu');
-    var cont = document.querySelector('.first-part__content');
-    if(window.pageYOffset > 400){
-        header.classList.add('active');
-        cont.classList.add('move');
-    } else{
-        header.classList.remove('active');
-        cont.classList.remove('move');
-    }
+var popUp = document.getElementsByClassName("popup");
+function Activate() {
+    popUp.classList.add("active");
 }
+
